b8fe0b652156eeb216572008dcf0aabe09e6f02e
taichi0129 <nishi.made.t.9210@gmail.com>
2020-06-07 19:04:25 +0900

    little fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
+  devise_for :users
   # devise_for :admins
   root to: "users#index"
   namespace :skills do
