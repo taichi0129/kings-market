3c2fc902baab0510945cfb6ba123bb5013289289
taichi0129 <nishi.made.t.9210@gmail.com>
2020-06-10 12:29:35 +0900

    検索機能の修正

--- app/controllers/skills/searches_controller.rb
+++ app/controllers/skills/searches_controller.rb
@@ -1,6 +1,6 @@
 class Skills::SearchesController < ApplicationController
   def index
-    @skill = Skill.find_by(date: params[:date])
+    @skill = Skill.find_by(params[:id])
     gon.skill = @skill
     @comment = Comment.new
     @comments = @skill.comments.includes(:user)
